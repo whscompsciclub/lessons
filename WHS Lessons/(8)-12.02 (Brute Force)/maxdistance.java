8ad23f1bfd459eef509de0346062b802a6180660
Ericspk33 <38512469+Ericspk33@users.noreply.github.com>
2020-12-02 18:43:15 -0800

    Update maxdistance.java

--- WHS Lessons/(8)-12.02 (Brute Force)/maxdistance.java
+++ WHS Lessons/(8)-12.02 (Brute Force)/maxdistance.java
@@ -6,7 +6,7 @@ import java.util.regex.*;
 
 public class maxdistance {
     public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
       	int n = in.nextInt();
     	// Start your code here
         
