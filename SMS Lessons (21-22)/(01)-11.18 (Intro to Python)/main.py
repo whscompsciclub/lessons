bfdc0d4d2416612e6ce51a65b1b93770040da3ae
Ericspk33 <ericspk33@gmail.com>
2021-11-25 09:22:25 -0800

    add hours

--- SMS Lessons (21-22)/(01)-11.18 (Intro to Python)/main.py
+++ SMS Lessons (21-22)/(01)-11.18 (Intro to Python)/main.py
@@ -1,10 +1,13 @@
 # About Me Assignment
 
 name = str(input())
-print("My name is", name)
+print("My name is " + name)
 
 age = int(input())
-print("I am " + age + " years old")
+print("I am", age, "years old")
 
 hobby = str(input())
-print(hobby, "is one of my favorite hobbies")
+print("One of my favorite hobbies is", hobby)
+
+hours = int(input())
+print("I spend around", hours, "hours a day " + hobby)
