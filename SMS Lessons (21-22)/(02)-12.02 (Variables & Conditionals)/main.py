2cb4e08986e905f8485dcf6079f38d737b0d2dcf
Ericspk33 <ericspk33@gmail.com>
2022-01-06 16:42:20 -0800

    newline character

--- SMS Lessons (21-22)/(02)-12.02 (Variables & Conditionals)/main.py
+++ SMS Lessons (21-22)/(02)-12.02 (Variables & Conditionals)/main.py
@@ -1,4 +1,4 @@
-
+# \n is a newline character
 operation = int(input("Choose Operation:\n   1: addition\n   2: subtraction\n   3: multiplication\n   4: division\n"))
 num_1 = int(input("\nEnter first integer: "))
 num_2 = int(input("\nEnter second integer: "))
